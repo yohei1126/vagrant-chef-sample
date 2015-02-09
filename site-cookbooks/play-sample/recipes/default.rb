c9ee1bfc14a2eda06409d5374a47a5cd7a8f3f95
Yohei Onishi <vivre214@gmail.com>
2015-02-09 22:25:56 +0900

    tetst

--- site-cookbooks/play-sample/recipes/default.rb
+++ site-cookbooks/play-sample/recipes/default.rb
@@ -1,23 +1,3 @@
-postgresql_database 'create databases' do
-  connection(
-    :host     => '127.0.0.1',
-    :port     => 5432,
-    :username => 'postgres',
-    :password => node['postgresql']['password']['postgres']
-  )
-  database_name 'book'
-  sql "create table book (
-         id         bigint not null,
-         title      varchar(255) not null,
-         author     varchar(255) not null,
-         publisher  varchar(255) not null,
-         constraint pk_book primary key (id));
-       create sequence book_seq;
-       insert into book values('test book', 'test author', 'test publisher');
-      "
-  action :query
-end
-
 git '/home/vagrant/play-sample' do
   repository 'https://github.com/ogis-onishi/play-sample.git'
   user 'vagrant'
--- site-cookbooks/postgresql_config/recipes/default.rb
+++ site-cookbooks/postgresql_config/recipes/default.rb
@@ -1,10 +1,12 @@
+postgresql_connection_info = {
+  :host     => '127.0.0.1',
+  :port     => 5432,
+  :username => 'postgres',
+  :password => 'postgres'
+}
+
 postgresql_database 'sampledb' do
-  connection(
-    :host     => '127.0.0.1',
-    :port     => 5432,
-    :username => 'postgres',
-    :password => node['postgresql']['password']['postgres']
-  )
+  connection postgresql_connection_info
   template 'DEFAULT'
   encoding 'DEFAULT'
   tablespace 'DEFAULT'
@@ -12,3 +14,19 @@ postgresql_database 'sampledb' do
   owner 'postgres'
   action :create
 end
+
+postgresql_database 'create table' do
+  connection postgresql_connection_info
+  database_name 'book'
+  sql "create table book (
+         id         bigint not null,
+         title      varchar(255) not null,
+         author     varchar(255) not null,
+         publisher  varchar(255) not null,
+         constraint pk_book primary key (id));
+       create sequence book_seq;
+       insert into book values('test book', 'test author', 'test publisher');
+      "
+  action :query
+end
+
